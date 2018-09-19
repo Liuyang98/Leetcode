35613fd34ca2252a41f7f4ff2412cf51c1ab1d58
Liuyang <ly@zintow.com>
2018-09-19 17:31:17 +0800

    leetcode

--- app/src/main/java/com/ly/leetcode/MainActivity.java
+++ app/src/main/java/com/ly/leetcode/MainActivity.java
@@ -3,18 +3,16 @@ package com.ly.leetcode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.ly.leetcode.sub.Subject34;
+import com.ly.leetcode.sub.Subject55;
 
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Subject34().run();
+        new Subject55().run();
 
     }
 }
