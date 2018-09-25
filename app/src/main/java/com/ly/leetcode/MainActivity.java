7da3ab809eb8949d912af6b5d79f4635b7ef009e
Liuyang98 <449360145@qq.com>
2018-09-25 17:51:33 +0800

    remove .idea

--- app/src/main/java/com/ly/leetcode/MainActivity.java
+++ app/src/main/java/com/ly/leetcode/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Subject41().run();
+//        new Subject41().run();
     }
 }
