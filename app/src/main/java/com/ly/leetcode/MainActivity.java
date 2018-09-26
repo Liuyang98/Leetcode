bb853f34ac1536eafab65ad7f1f9886d1bf86aba
Liuyang98 <449360145@qq.com>
2018-09-26 17:55:07 +0800

    add 319

--- app/src/main/java/com/ly/leetcode/MainActivity.java
+++ app/src/main/java/com/ly/leetcode/MainActivity.java
@@ -3,7 +3,6 @@ package com.ly.leetcode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.ly.leetcode.sub.Subject41;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/ly/leetcode/sub/Subject319.java
@@ -0,0 +1,35 @@
+package com.ly.leetcode.sub;
+
+
+import android.util.Log;
+
+/**
+ * 灯泡开关
+ * <p>
+ * 初始时有 n 个灯泡关闭。 第 1 轮，你打开所有的灯泡。 第 2 轮，每两个灯泡你关闭一次。 第 3 轮，每三个灯泡切换一次开关（如果关闭则开启，如果开启则关闭）。第 i 轮，每 i 个灯泡切换一次开关。 对于第 n 轮，你只切换最后一个灯泡的开关。 找出 n 轮后有多少个亮着的灯泡。
+ * 示例:
+ * 输入: 3
+ * 输出: 1
+ * 解释:
+ * 初始时, 灯泡状态 [关闭, 关闭, 关闭].
+ * 第一轮后, 灯泡状态 [开启, 开启, 开启].
+ * 第二轮后, 灯泡状态 [开启, 关闭, 开启].
+ * 第三轮后, 灯泡状态 [开启, 关闭, 关闭].
+ * 你应该返回 1，因为只有一个灯泡还亮着。
+ */
+
+
+public class Subject319 implements Subjcet {
+    private static final String TAG = "Subject121";
+
+    @Override
+    public void run() {
+        int n = bulbSwitch(5);
+        Log.e(TAG, "运算结果: " + n);
+    }
+
+    public int bulbSwitch(int n) {
+   return (int) Math.sqrt(n);
+    }
+}
+
